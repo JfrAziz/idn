0ef80a01ee0e2b0a53145ed95de357aae4cab291
Jafar Aziz <aziz@jafaraziz.com>
2024-02-04 15:07:01 +0700

    feat: add color for each features

--- components/app.tsx
+++ components/app.tsx
@@ -1,10 +1,35 @@
 import { Maps } from "./maps";
+import { Button } from "./ui/button";
+import { useMapStore } from "./store";
+import { ChevronRight } from "lucide-react";
 
 export const App = () => {
-  return (
-    <div className="relative h-svh max-h-svh ">
-      <Maps />
-    </div>
+  const source = useMapStore((state) => state.source);
 
+  return (
+    <div className="relative h-svh max-h-svh flex flex-col">
+      <header className="h-12 border-b border-border shadow-lg flex items-center p-2">
+        <div className="flex items-center gap-2 uppercase text-sm font-mono">
+          <Button
+            size="sm"
+            variant="ghost"
+            onClick={() =>
+              useMapStore.setState({ source: { source: "34-bps" } })
+            }
+          >
+            {source.source}
+          </Button>
+          <ChevronRight className="size-4" />
+          {source.province && (
+            <Button variant="ghost" size="sm">
+              {source.province}
+            </Button>
+          )}
+        </div>
+      </header>
+      <div className="flex-1 relative w-full">
+        <Maps />
+      </div>
+    </div>
   );
 };
--- components/maps/geojson.tsx
+++ components/maps/geojson.tsx
@@ -1,6 +1,7 @@
 import useSWR from "swr";
-import { useEffect } from "react";
+import chroma from "chroma-js";
 import { GeoJSON } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useMapStore } from "@components/store";
 import { INDONESIA_BOUNDING_BOX } from "./config";
 import { FeatureGroup, useMap } from "react-leaflet";
@@ -11,32 +12,38 @@ export const GeoJSONLayer = () => {
 
   const source = useMapStore((state) => state.source);
 
-  const { data } = useSWR(
-    getHierarichalGeoJSONURL(source),
-    geoJSONfetcher
-  );
+  const [color, setColor] = useState<string[]>([]);
+
+  const { data } = useSWR(getHierarichalGeoJSONURL(source), geoJSONfetcher);
 
   useEffect(() => {
     const bounds = useMapStore.getState().geoJSONref?.getBounds();
 
     if (bounds && Object.keys(bounds).length !== 0) map.fitBounds(bounds);
     else map.fitBounds(INDONESIA_BOUNDING_BOX);
+
+    if (data) setColor(chroma.scale("OrRd").colors(data.features.length));
   }, [data]);
 
   return (
     <FeatureGroup ref={(ref) => useMapStore.setState({ geoJSONref: ref })}>
-      {data?.features.map((feature) => (
+      {data?.features.map((feature, idx) => (
         <GeoJSON
           data={feature}
           key={feature.properties.id}
-          style={{ opacity: 1, dashArray: "0" }}
+          style={{
+            opacity: 1,
+            weight: 0.5,
+            color: "black",
+            dashArray: "0",
+            fillColor: color[idx],
+          }}
           onEachFeature={(f, l) =>
-            l.on("click", () => {
-              console.log(f.properties)
+            l.on("click", () =>
               useMapStore.setState({
-                source: { source: "34-bps", province: f.properties.id },
-              });
-            })
+                source: { source: "34-bps", province: f.properties.province },
+              })
+            )
           }
         />
       ))}
